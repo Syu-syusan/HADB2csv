82cac2bca5f3c2ff8d61b136239a807e7741b3da
Syu-syusan <syu_syusan@outlook.jp>
2024-07-30 01:34:18 +0900

    edit csv

--- HADB2csv.py
+++ HADB2csv.py
@@ -1,12 +1,11 @@
 import sqlite3
-import pandas as pd
 from datetime import datetime
 import openpyxl
 
 conn = sqlite3.connect('/usr/share/hassio/homeassistant/home-assistant_v2.db')
 cursor = conn.cursor()
 
-metadata_ids = [15, 16, 12, 13, 17, 18, 19, 42, 43, 44, 45, 46, 47]
+metadata_ids = [15, 16, 12, 13, 17, 18, 19, 42, 43, 44, 45, 46, 54]
 
 current_time = datetime.now()
 current_timestamp = current_time.replace(minute=(current_time.minute // 30) * 30, second=0, microsecond=0)
@@ -36,6 +35,7 @@ for metadata_id in metadata_ids:
 conn.close()
 
 print(new_data)
+
 sheet.cell(row=5, column=1, value=new_data[0]['日時'])
 for index, data in enumerate(new_data):
     row_num = 1 + index
