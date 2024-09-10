24590882fe6f440d28151c18bd8d3c9aeb7b3b5a
Syusyusan <74150680+Syu-syusan@users.noreply.github.com>
2024-09-10 23:19:58 +0900

    Update HADBadd.py

--- HADBadd.py
+++ HADBadd.py
@@ -86,8 +86,8 @@ def fetch_and_insert_data():
             target_conn.close()
 
 # スケジュール設定
-schedule.every().hour.at(":06").do(fetch_and_insert_data)  # 毎時00分に実行
-schedule.every().hour.at(":38").do(fetch_and_insert_data)  # 毎時30分に実行
+schedule.every().hour.at(":09").do(fetch_and_insert_data)  # 毎時00分に実行
+schedule.every().hour.at(":39").do(fetch_and_insert_data)  # 毎時30分に実行
 
 # スケジュールを永続的に実行する
 while True:
